c633a259dc8ded13f51315adf61914e78703f05b
Ayrton Everton <ayrtoneverton@gmail.com>
2019-05-05 22:49:40 -0300

    Criada a estrutura de navegação entre os níveis do game. close #3

--- App.js
+++ App.js
@@ -1,14 +1,39 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, StyleSheet, Text, View } from 'react-native';
+
+const levels = [];
 
 export default class App extends React.Component {
 	constructor(props) {
 		super(props);
+		this.state = { level: 1 };
+		this.nextLevel = this.nextLevel.bind(this);
+		this.prevLevel = this.prevLevel.bind(this);
+	}
+	help(){
+		Alert.alert('Help', 'Decifre o enigma para passar para o próximo.');
+	}
+	getLevel(){
+		const Level = levels[this.state.level - 1];
+		if(Level)
+			return <Level nextLevel={this.nextLevel}/>;
+		else
+			Alert.alert('Finish', 'Parabéns, conseguiu chegar ao fim.');
+	}
+	nextLevel(){
+		this.setState({ level: this.state.level + 1 });
+	}
+	prevLevel(){
+		if(this.state.level > 1)
+			this.setState({ level: this.state.level - 1 });
 	}
 	render() {
 		return (
 			<View style={styles.container}>
-				<Text>Hello World</Text>
+				{this.getLevel()}
+				<Text style={styles.prev} onPress={this.prevLevel}>&laquo;</Text>
+				<Text style={styles.level}>{this.state.level}</Text>
+				<Text style={styles.help} onPress={this.help}>?</Text>
 			</View>
 		);
 	}
@@ -17,5 +42,35 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1
+	},
+	prev:{
+		position: 'absolute',
+		bottom: 5,
+		right: 130,
+		fontSize: 28,
+		paddingVertical: 10,
+		paddingHorizontal: 22,
+		borderRadius: 100,
+		backgroundColor: '#ddd'
+	},
+	level:{
+		position: 'absolute',
+		bottom: 5,
+		right: 65,
+		fontSize: 28,
+		paddingVertical: 10,
+		paddingHorizontal: 22,
+		borderRadius: 100,
+		backgroundColor: '#ddd'
+	},
+	help:{
+		position: 'absolute',
+		bottom: 5,
+		right: 5,
+		fontSize: 28,
+		paddingVertical: 10,
+		paddingHorizontal: 22,
+		borderRadius: 100,
+		backgroundColor: '#ddd'
 	}
 });
