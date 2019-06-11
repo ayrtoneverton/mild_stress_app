7bb7543b2cf25f619e1cf445df848c9bfcaf8160
Ayrton Everton <ayrtoneverton@gmail.com>
2019-06-11 13:48:17 -0300

    Romovendo a pasta src para executar no MS App Center

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Alert, StyleSheet, Text, View } from 'react-native';
 import FS from 'react-native-fs';
-import Level1 from './src/Level1';
-import Level2 from './src/Level2';
-import Level3 from './src/Level3';
-import EndGame from './src/EndGame';
+import Level1 from './Level1';
+import Level2 from './Level2';
+import Level3 from './Level3';
+import EndGame from './EndGame';
 
 const levels = [Level1, Level2, Level3, EndGame];
 const fileLevel = FS.ExternalDirectoryPath +'/ms-lv';
